e93ec4dffb801d4ebfd170a97da636d21b540904
Max DeVos <devosmaxwell@gmail.com>
2021-06-14 15:28:17 -0500

    Imported Empty Springboot Project

---
+++ src/main/java/com/torpedobags/TorpedoBagsManager/TorpedoBagsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.torpedobags.TorpedoBagsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorpedoBagsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorpedoBagsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torpedobags/TorpedoBagsManager/TorpedoBagsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torpedobags.TorpedoBagsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorpedoBagsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
